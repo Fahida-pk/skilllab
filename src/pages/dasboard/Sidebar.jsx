da12a5e7e541654343ed33034bd8ce0d4d26cff0
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-04-10 15:09:32 +0530

    update project

--- src/pages/dasboard/Sidebar.jsx
+++ src/pages/dasboard/Sidebar.jsx
@@ -7,8 +7,12 @@ function Sidebar() {
   return (
     <div className="sidebar">
 
-      <h2 className="logo">SkillLab</h2>
+      {/* TOP SECTION */}
+      <div className="top">
+        <h2 className="logo">SkillLab</h2>
+      </div>
 
+      {/* BOTTOM PROFILE */}
       <div className="profile">
         {user?.picture ? (
           <img src={user.picture} alt="profile" className="profile-img" />
@@ -16,8 +20,8 @@ function Sidebar() {
           <FaUserCircle className="profile-icon" />
         )}
 
-        <p className="name">{user?.name}</p>
-        <p className="email">{user?.email}</p>
+        <p className="name">{user?.name || "User Name"}</p>
+        <p className="email">{user?.email || "user@email.com"}</p>
       </div>
 
     </div>
