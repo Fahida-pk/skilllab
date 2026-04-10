ca96f6941dc4c7505f9367b7bb7a2956e859d7bd
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-04-10 16:40:54 +0530

    update project

--- src/pages/dasboard/Sidebar.jsx
+++ src/pages/dasboard/Sidebar.jsx
@@ -7,28 +7,25 @@ function Sidebar() {
   return (
     <div className="sidebar">
 
-      {/* 🔝 TOP */}
-      <div className="top">
-        <h2 className="logo">SKILL LAB</h2>
-      </div>
+  {/* TOP */}
+  <div className="top">
+    <h2 className="logo">SkillLab</h2>
+  </div>
 
-      {/* 🔽 BOTTOM PROFILE */}
-      <div className="profile">
-        {user?.picture ? (
-          <img src={user.picture} alt="profile" className="profile-img" />
-        ) : (
-          <FaUserCircle className="profile-icon" />
-        )}
-
-        <p className="name">{user?.name}</p>
-        <p className="email">{user?.email}</p>
-
-        <button className="logout-btn">
-          <FaSignOutAlt /> Logout
-        </button>
-      </div>
-
+  {/* BOTTOM */}
+  <div className="bottom">
+    <div className="profile">
+      <FaUserCircle className="icon" />
+      <p>{user?.name}</p>
+      <span>{user?.email}</span>
     </div>
+
+    <button className="logout" onClick={handleLogout}>
+      Logout
+    </button>
+  </div>
+
+</div>
   );
 }
 
