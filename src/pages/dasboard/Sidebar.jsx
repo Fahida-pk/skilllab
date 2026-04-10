ebedd3bc2491834b89b073a2efcb73a432b4528a
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-04-10 16:46:07 +0530

    update project

--- src/pages/dasboard/Sidebar.jsx
+++ src/pages/dasboard/Sidebar.jsx
@@ -1,6 +1,5 @@
 import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
 import "./sidebar.css";
-
 function Sidebar() {
   const user = JSON.parse(localStorage.getItem("user"));
 
@@ -12,20 +11,22 @@ function Sidebar() {
         <h2 className="logo">SKILL LAB</h2>
       </div>
 
-      {/* 🔽 BOTTOM PROFILE */}
-      <div className="profile">
-        {user?.picture ? (
-          <img src={user.picture} alt="profile" className="profile-img" />
-        ) : (
-          <FaUserCircle className="profile-icon" />
-        )}
+      {/* 🔽 BOTTOM (🔥 ADD THIS WRAPPER) */}
+      <div className="bottom">
+        <div className="profile">
+          {user?.picture ? (
+            <img src={user.picture} alt="profile" className="profile-img" />
+          ) : (
+            <FaUserCircle className="profile-icon" />
+          )}
 
-        <p className="name">{user?.name}</p>
-        <p className="email">{user?.email}</p>
+          <p className="name">{user?.name}</p>
+          <p className="email">{user?.email}</p>
 
-        <button className="logout-btn">
-          <FaSignOutAlt /> Logout
-        </button>
+          <button className="logout-btn">
+            <FaSignOutAlt /> Logout
+          </button>
+        </div>
       </div>
 
     </div>
