23f61d39ac0d60f2b8947494d516fe6a175ee5f9
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-04-10 14:11:37 +0530

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/login.jsx";
-
+import Dashboard from "./pages/dashboard/Dashboard.jsx";
 
 function App() {
   return (
     <Routes>
-
-      {/* Default redirect */}
       <Route path="/" element={<Navigate to="/login" />} />
-
-      {/* Login */}
       <Route path="/login" element={<Login />} />
 
-
+      {/* ✅ ADD THIS */}
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
