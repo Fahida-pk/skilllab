3964428ee6022e15d56af1722dd58685f481465f
Fahida-pk <118457841+Fahida-pk@users.noreply.github.com>
2026-09-17 13:00:40 +0530

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,82 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/login.jsx";
 import Dashboard from "./pages/dasboard/Dashboard.jsx";
 import Task from "./pages/task/task.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
 
 // Admin
 import AdminLogin from "./pages/admin/AdminLogin.jsx";
+
 
 function App() {
   return (
     <Routes>
 
-      {/* ==============================
+      {/* =========================================
           DEFAULT
-      ============================== */}
+      ========================================= */}
+
       <Route
         path="/"
-        element={<Navigate to="/login" />}
+        element={
+          <Navigate
+            to="/login"
+            replace
+          />
+        }
       />
 
 
-      {/* ==============================
-          STUDENT
-      ============================== */}
+      {/* =========================================
+          STUDENT LOGIN
+      ========================================= */}
+
       <Route
         path="/login"
         element={<Login />}
       />
 
-      <Route
-        path="/dashboard"
-        element={<Dashboard />}
-      />
 
-      <Route
-        path="/task"
-        element={<Task />}
-      />
+      {/* =========================================
+          PROTECTED STUDENT ROUTES
+      ========================================= */}
+
+      <Route element={<ProtectedRoute />}>
+
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        <Route
+          path="/task"
+          element={<Task />}
+        />
+
+      </Route>
 
 
-      {/* ==============================
-          ADMIN
-      ============================== */}
+      {/* =========================================
+          ADMIN LOGIN
+      ========================================= */}
+
       <Route
         path="/admin/login"
         element={<AdminLogin />}
+      />
+
+
+      {/* =========================================
+          UNKNOWN URL
+      ========================================= */}
+
+      <Route
+        path="*"
+        element={
+          <Navigate
+            to="/login"
+            replace
+          />
+        }
       />
 
     </Routes>
